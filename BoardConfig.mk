37b56fb5d9658cc75340f23b87c3766f843bbd78
lolipuru <banstellazz@gmail.com>
2024-06-15 20:10:46 +0700

    pdx223: use prebuilt kernel for now
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,11 +18,26 @@
 -include device/sony/sm8450-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/sony/pdx223
+KERNEL_PATH := $(DEVICE_PATH)-kernel
 
 # Display
 TARGET_SCREEN_DENSITY := 420
 
+# Dtb/o
+BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbo.img
+BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtb
+
+# Kernel
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/vendor_ramdisk/modules.load))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(KERNEL_PATH)/vendor_ramdisk/modules.blocklist
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/vendor_ramdisk/modules.load.recovery))
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/vendor_dlkm/modules.load))
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE :=  $(KERNEL_PATH)/vendor_dlkm/modules.blocklist
+
 BOARD_KERNEL_CMDLINE += buildproduct=pdx223
+TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
+PRODUCT_COPY_FILES += \
+	$(KERNEL_PATH)/kernel:kernel
 
 # Props
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
