45af356ea9172d2daa8d7dd96f83e209abdfd13c
Rinigus <rinigus.git@gmail.com>
2025-02-04 21:41:03 +0200

    pdx223: drop importing vendor.qti.hardware.display.config-V2-ndk_platform

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/libcamximageformatutils.so)
+            [ "$2" = "" ] && return 0
+            ${PATCHELF} --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
